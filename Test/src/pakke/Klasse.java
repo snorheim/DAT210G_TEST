1874c9b48de95ee3a28f32e57fd5f240a97ff159
snorheim <snorheim@gmail.com>
2014-01-24 15:46:48 +0100

    Første utkast.
    Blei litt mye mapper inni mapper her. Dette får vi finne ut av når vi
    lager den skikkelige.

---
+++ Test/src/pakke/Klasse.java
@@ -0,0 +1,15 @@
+package pakke;
+
+public class Klasse {
+	private int AnswerToLifeUniverseAndEverything = 42;
+
+	public Klasse(){
+		System.out.print(AnswerToLifeUniverseAndEverything);
+	}
+	
+	public static void main(String[] args) {
+		Klasse k = new Klasse();
+
+	}
+
+}
