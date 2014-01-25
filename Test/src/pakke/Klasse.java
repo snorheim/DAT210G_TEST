a40c7394dd1499e48318f1917307618f85572e17
simjes <s.jespersen@stud.uis.no>
2014-01-25 01:46:30 +0100

    Jframe added

--- Test/src/pakke/Klasse.java
+++ Test/src/pakke/Klasse.java
@@ -1,5 +1,11 @@
 package pakke;
 
+import java.awt.FlowLayout;
+import java.awt.Font;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 public class Klasse {
 	private int AnswerToLifeUniverseAndEverything = 42;
 
@@ -11,8 +17,15 @@ public class Klasse {
 	}
 	
 	public static void main(String[] args) {
-		Klasse k = new Klasse();
-		System.out.print(k.getAnswer());
+		JFrame jf = new JFrame("AnswerToLifeUniverseAndEverything");
+		jf.setLayout(new FlowLayout());
+		JLabel jl = new JLabel(Integer.toString(new Klasse().getAnswer()));
+		jl.setFont(new Font("Serif", Font.PLAIN, 200));
+		jf.add(jl);
+		jf.setSize(400, 300);
+		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jf.setLocationRelativeTo(null);
+		jf.setVisible(true);
 
 	}
 
