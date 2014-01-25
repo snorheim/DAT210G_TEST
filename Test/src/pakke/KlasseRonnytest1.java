5d87cadc323dfaf294c487d447521064f6887c3b
Ronny Wathne <spacew@gmail.com>
2014-01-25 10:03:07 +0100

    Ronny testklasse, testpakke

---
+++ Test/src/pakke/KlasseRonnytest1.java
@@ -0,0 +1,17 @@
+package pakke;
+
+public class KlasseRonnytest1 {
+
+	public static boolean metode1 (int a){
+		return a==0;
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		boolean a = metode1(0);
+		System.out.print(a);
+		
+	}
+
+}
