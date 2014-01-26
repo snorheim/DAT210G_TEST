50db16af3934e5428cc232c38adb5f7ac72cd577
javaronnie <ronnie.bjorkelund@gmail.com>
2014-01-26 23:49:34 +0100

    enda en commit
    afgadgsdg

---
+++ Test/src/annenpakke/class.java
@@ -0,0 +1,19 @@
+package annenpakke;
+
+public class Pear {
+	private int weight = 42;
+
+	public Pear(){
+		
+	}
+	public int getWeight(){
+		return weight;
+	}
+	
+	public static void main(String[] args) {
+		Pear p = new Pear();
+		System.out.print("Pæra veier %s kilo!", p.getWeight());
+
+	}
+
+}
