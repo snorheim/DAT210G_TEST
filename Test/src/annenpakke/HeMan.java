e2807f1181f28861165c5cad72cb5b600c93676b
javaronnie <ronnie.bjorkelund@gmail.com>
2014-01-29 12:32:02 +0100

    Added feature
    new feature

---
+++ Test/src/annenpakke/HeMan.java
@@ -0,0 +1,12 @@
+package annenpakke;
+
+public class HeMan {
+
+	private int strength = 100000;
+	private int life = 100000;
+	
+	public HeMan() {
+		System.out.println("I got the power!");
+	}
+	
+}
