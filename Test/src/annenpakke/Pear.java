646adcb27d4b0f9bb2f08c12d142cc9ab9dc4931
Kjelli <kjehe91@gmail.com>
2014-01-29 09:27:04 +0100

    Paerefila jaa

--- Test/src/annenpakke/Pear.java
+++ Test/src/annenpakke/Pear.java
@@ -3,16 +3,17 @@ package annenpakke;
 public class Pear {
 	private int weight = 42;
 
-	public Pear(){
-		
+	public Pear() {
+
 	}
-	public int getWeight(){
+
+	public int getWeight() {
 		return weight;
 	}
-	
+
 	public static void main(String[] args) {
 		Pear p = new Pear();
-		System.out.print("Pæra veier %s kilo!", p.getWeight());
+		System.out.printf("Paera veier %s kilo!", p.getWeight());
 		int d = 2;
 		System.out.print("hllogefgdgf");
 
